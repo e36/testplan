20c3e75efdaeb1a3c18811da5622aef2a0bc6f14
e36 <agent4159@gmail.com>
2021-12-31 14:08:04 -0600

    modifications

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace testplan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting testplan");
+
+            // test for arguments, we'll modify this later to parse them
+            // Not sure yet what kind or args we'll use, but I'd like to have one for auto-generating
+            // the files needed to run a test
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments");
+            } else {
+                Console.WriteLine($"{args.Length} arguments found!");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+
+
         }
     }
 }
